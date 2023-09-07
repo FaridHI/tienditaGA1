55d262db7d9518f2292725879fc84afd05175891
FaridHI <1121041@senati.pe>
2023-09-07 18:30:42 -0500

    guarde datos

---
+++ SQLQuery4.sql
@@ -0,0 +1,11 @@
+CREATE TABLE USUARIO (
+	id INT PRIMARY KEY,
+	usuario VARCHAR(55),
+	Contraseña VARCHAR(55)
+);
+
+Select * from USUARIO
+
+insert into USUARIO(id, usuario, Contraseña) values (1, 'info1','data2')
+
+update USUARIO set id = 'ddx',usuario = 'ddx' where Contraseña =¿+
